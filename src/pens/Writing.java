a6899445e7b21aeb73df662194eef8ae4c7a28b8
Chintalarajesh <109692986+Chintalarajesh@users.noreply.github.com>
2022-08-03 19:55:54 +0530

    Second commit

--- src/pens/Writing.java
+++ src/pens/Writing.java
@@ -3,5 +3,14 @@ package pens;
 public class Writing {
 public static void main(String[] args) {
 	System.out.println("my name is Rajesh");
+    
+	
+	
+	System.out.println("a");
+	System.out.println("b");
+	System.out.println("c");
+	
+	
+	
 }
 }
